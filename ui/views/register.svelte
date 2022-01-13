7f41b79abbcce165aa1a6beb28210eb4b041cf03
UbuntuDell2 <nathan.chevalier@epita.fr>
2022-01-13 06:28:48 +0100

    Add egister view

---
+++ ui/views/register.svelte
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr" >
+    <head>
+        <meta charset="utf-8">
+        <title>My Little Intra</title>
+        <meta name="description" content="Nous tentons d'avoir les cefs de compréhension à la fois technique et théorique de ce que constitue un intra">
+        <meta name="keywords" content="Nous tentons d'avoir les cefs de compréhension à la fois technique et théorique de ce que constitue un intra"/>
+        <meta name="author" content="" />
+        <meta name="theme-color" content="#333333">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" type="/image/png" href="/img/icon.png" >
+        <link rel="stylesheet" href="/css/home.css">
+        <link rel="stylesheet" href="/css/header.style.css">
+    </head>
+
+    <body>
+
+    
+
+    </body>
+</html>
